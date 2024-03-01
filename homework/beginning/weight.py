066c93c59e576c5e7acc7867aab11da875af97e3
Steven <106290011+adrian0671232@users.noreply.github.com>
2024-03-01 15:09:27 -0500

    Create weight.py

---
+++ homework/beginning/weight.py
@@ -0,0 +1,21 @@
+print("Please enter your weight (in lbs):")
+weight = int(input())
+kilo = weight * 2.205
+
+earth = 1
+mercury = 0.4
+venus = 0.9
+mars = 0.38
+jupiter = 2.3
+saturn = 1.1
+uranus = 0.92
+neptune = 1.2
+
+print("Your weight on Earth would be: " + str(earth * weight) + " lbs (" + str(kilo * earth) + "kg)")
+print("Your weight on Mercury would be: " + str(mercury * weight) + " lbs (" + str(kilo * mercury) + "kg)")
+print("Your weight on Venus would be: " + str(venus * weight) + " lbs (" + str(kilo * venus) + "kg)")
+print("Your weight on Mars would be: " + str(mars * weight) + " lbs (" + str(kilo * mars) + "kg)")
+print("Your weight on Jupiter would be: " + str(jupiter * weight) + " lbs (" + str(kilo * jupiter) + "kg)")
+print("Your weight on Saturn would be: " + str(saturn * weight) + " lbs (" + str(kilo * saturn) + "kg)")
+print("Your weight on Uranus would be: " + str(uranus * weight) + " lbs (" + str(kilo * uranus) + "kg)")
+print("Your weight on Neptune would be: " + str(neptune * weight) + " lbs (" + str(kilo * neptune) + "kg)")
